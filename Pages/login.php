938a9b32d2ed5454cd2e0d6cb5b914a39d9da28e
Than Van Ky <thanky4444.com>
2025-03-10 15:25:30 +0700

    initial UI

---
+++ Pages/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div >
+            <h1>LOG IN</h1>
+            <input type="text">
+            <input type="text">
+            <button></button>
+            <div>
+                <p></p>
+                <a href=""></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
